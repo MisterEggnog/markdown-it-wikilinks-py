fa6385cad3aa026145f15c372d365bd89ee54e95
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-10-11 19:46:26 -0700

    parses the regex

--- markdown_it_wikilinks/__init__.py
+++ markdown_it_wikilinks/__init__.py
@@ -1,3 +1,22 @@
+import re
+
+_wikilink_regex = r"\[\[([^|\]\n]+)(\|([^\]\n]+))?\]\]"
+
+
 def wikilinks(self, tokens, idx, options, env):
+    def generate_page_path_from_label(s):
+        return s
+
+    if re_match := _wikilink_regex.match(tokens.attr["src"]):
+        label = None
+        page_path = None
+        htmlAttrs = []
+        htmlAttrsString = ""
+        if len(re_match) == 3:
+            label = match[3]
+            pagePath = match[1]
+        else:
+            label = match[1]
+            pagePath = generate_page_path_from_label(match[1])
     # Does nothing?
     return self.renderToken(tokens, idx, options, env)
